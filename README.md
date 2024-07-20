6f1c03b6284c9cf4c949060a9a1441fe7fd1ab5b
Sahiljit <sahiljit3@gmail.com>
2024-07-20 15:24:00 +0530

    feat(4.0.0) - component and styles works with radix/theme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 ## @sahiljit/ui
+
+### Installation
+
+1.  Run the following command -
+    `npm i @sahiljit/ui`
+
+2.  Inside layout.tsx import the following code -
+    `import "@sahiljit/ui/dist/style.css";
+import { Theme } from "@radix-ui/themes";`
+
+3.  Wrap children in layout.tsx inside `<Theme>{children}</Theme>`
--- lib/radix/button/index.tsx
+++ lib/radix/button/index.tsx
@@ -1,16 +1,10 @@
 export interface ButtonProps {}
-import { Theme, Button as RadixButton } from "@radix-ui/themes";
+import { Button as RadixButton } from "@radix-ui/themes";
 
 export const Button = ({}: ButtonProps) => {
   return (
-    <Theme
-      accentColor="mint"
-      grayColor="gray"
-      panelBackground="solid"
-      scaling="100%"
-      radius="full"
-    >
-      <RadixButton variant="soft">Sahil</RadixButton>
-    </Theme>
+    <RadixButton variant="soft" className="!bg-red-300">
+      Sahil
+    </RadixButton>
   );
 };
